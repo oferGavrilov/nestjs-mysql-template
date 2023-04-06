30873cc828f43b62e93a5d508d3d62148344a158
Ofer Gavrilov <ofergavri@gmail.com>
2023-04-06 23:57:49 +0300

    added controllers & services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+
+const ormOptions: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '123456',
+  database: 'nestjs',
+  autoLoadEntities: true,
+  synchronize: true
+}
 @Module({
-  imports: [],
+  imports: [
+    TodoModule,
+    TypeOrmModule.forRoot(ormOptions)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
